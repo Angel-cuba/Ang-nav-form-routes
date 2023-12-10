ac3586bc77f04720823df120d71f429226654fe3
Angel_PC <araozangel842@gmail.com>
2023-12-11 01:30:11 +0200

    Add styles, navbar routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,38 @@ export const routes: Routes = [
         (m) => m.DashboardComponent
       );
     },
+    children: [
+      {
+        path: 'users',
+        loadComponent() {
+          return import('./components/users/users/users.component').then(
+            (m) => m.UsersComponent
+          );
+        },
+      },
+      {
+        path: 'about',
+        loadComponent() {
+          return import('./components/about/about.component').then(
+            (m) => m.AboutComponent
+          );
+        },
+      },
+      {
+        path: 'contact',
+        loadComponent() {
+          return import('./components/contact/contact.component').then(
+            (m) => m.ContactComponent
+          );
+        },
+      },
+
+      {
+        path: '',
+        redirectTo: 'users',
+        pathMatch: 'full',
+      },
+    ]
   },
   {
     path: '',
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  public navItems: any = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route.path !== '')
+    .filter((route) => !route.path?.includes(':'))
 }
